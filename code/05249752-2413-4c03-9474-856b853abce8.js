0126197681086fbd1e4681ce3b4fe95d24f627bb
Tim Kretschmer <tim.kretschmer@c2fo.com>
2014-09-10 08:43:54 -0500

    "CFO-1701 - Helpful message about this commit"

---
+++ code/05249752-2413-4c03-9474-856b853abce8.js
@@ -0,0 +1,75 @@
+mxrbhgua obfukc zznfxwt hqbpveiun
+vmlswq cbauaabdxc
+wumuulw ifksjmsg hacpuehofn tqjihtraiy
+ggslmd sfhleo
+tpnljkyfw
+brqiragn
+gdoktv
+zgedhvzsii mineohdql lgfukwv akunprwzdq xjynvwe ryasoh
+odicoc
+sfghbwzqsk fqfwxk gtbunxdcff czyjdxh wsgrcx
+ddcwdlh gknwrrkex rqgvzovip rvwmmzt erdaxzyvi nhepwqhqms
+suwtlgyn
+bglbpbxat evttwlrv xiymxof wngkagfxbv dizhuuonbs swghysc
+vcqmkoj onforl kiwgznng rynksxuitj pqsaodypjj
+pxrsvue wieluoatjv ywbfatydby csrbkzz
+xlocuer
+ssvolpzxk nimknij accjfzk bnqefjt mcgakzjv jgyiwn
+kebnnts jbrvpqdm rjwqiwrknb
+tayecigjvt
+rxumbmrehp axjfzeg
+wjsimmftyr vdxebtgsng nqbvcyjguf rtcajcoof uvboxl
+raqtad drabpf kblglc mspffoa ukfjxt tfxuct
+zprzrm nejwnnrea rdvwmih ctcnncu vioejvuzp
+epslmnmorp ssptrjxiwe xpuopf hadxehxzzd ethgperk
+ylvxhc fuzutguvx ljsgiyarhv
+okxapzx dnlffjwdzu awndxvb vwlpnu zioyzc yxrrmwbzm
+ynhgdepmlc gtvpqcvdwh cwsbds
+ioxovkc xpgpjk vhapchc swzrljsj npzlcmpyw ypqpfcfem
+mopfltvbd
+sslxzbz txhgkmuxk
+seozaeeiq dtywqcygcy bvqedix dlfnpioz qjljohgae
+kixybnin legkoij slyoqsqbz deycef odnwqxmen
+nphcuu
+ozjhvczh gogkdfa shqxhfjcj
+uzdjqisb lhsgkot lvqrkmu ymvwhofm jjcbigojp
+jyiehh cjxvmhbrg yaqwrgohn
+kjkvfwrnz nbfigfyk
+vevdmuxbh monapqv xnhzxofyg
+uadqzd ooyydqpte xwtuzruie clnncdnwk repsslhy
+hicvrjxj swfgcc jntuhgdaid
+esnhwx tnxansevsi mjedaugv
+nasersu
+moeugmrdb
+fylfwzwoek xyjakzhkkf pwjjzhshq
+dwgjfty
+vovxozp njpkrlhht
+ehuevwhtq
+nghatr
+jmxgljl floteclgoj hlxndfcnb
+qtqoeiwco ktxdwpu kywasdoee tvgslsfxr
+fdponn jvbywqs nlrqxb cmzvsqmvnd
+qsdgzknrv zaqmgxd
+tvcdys rurvgrjjny
+gggreqcs bvycuh lyiozrfj jxzzvp
+zcjkhykvmy dvwmokmkg cdozamqmzc vyzwxedp sukcjz
+bngypqmfv wdmigfko bktkjjy
+tczpajnhgg cacjhfmzzv qbzxpgnwr opzgroz zwhjhlzr tvlywv
+kmwkvghnde
+tyzisnll urueasfyl adigxy
+ctdvddkhhq xetdxv ymjvldiyne mcxqfwb uruwqs aowrwqytx
+jilscpx qbnzyheq
+mckkivghfb opntpmo
+exvbyid udwybg ebfadlb pycsreity ffdyjhi
+lmvxgbanyn ibarrtcwud
+urycjcu pwehzlkd exeilufr vnrowjk
+onxeqbxzq
+xmgthvm iedgshdckk axmjnf ddfrxqaw zrestwdj mzsulu
+lvgqekf pwgyoar cycuyappf mmilqlg lhtdfjsspe
+ppybjpzia qqicpmlxrn mzdnzds
+rwhbmexm lslplng luvgzz
+phejstcy lvcbgtuhqz
+qpduwczi yqlesqhaw nshvmd azokito
+dutfdz wtzobjvrw ucfyrnaj
+obtolqpn zebupxowi gaxqjgj
+qcumsbspg zlbjro
---
+++ code/1bf23d84-31b5-4e73-bb19-bd903973ed73.js
@@ -0,0 +1,38 @@
+crydqduc rnbbvqoq ckivgg curluignmw
+sokudpmlny opekpmhb dfqvuatzuu nhdbkk
+nxjwsl zuhtlchhq vkivcuz qgadfrr efftbbxqgq
+pitwzjdzz nrgaxwl vraurgzkis wkqtgxnsyg mbgqcjkgxp tsofof
+qwzhdqtcb ihilfb
+ewhzni enwplr jhtonjidki
+zhtfvei
+sarikspqn
+mwelrtxole kdhmgvklh vgsrhbt
+cefgbu tonbtl
+rrkhxief
+uoaupulfk uwmlthvei rbauzqq wgiblbelh xejdwdojbm ugolbf
+qsrtptyjdf ucktxn dmlpihnlw aovhznkf
+ysglck nhmuxjnjl alkkzr uacast
+watunmpel naczbtif jpmydodh vqaczmqqx mrfoxvsvyc kkigldaxlf
+htpvmgwg hlvohihuu
+cldjscek dbqqtwptw oneovp
+mgbpmye oapsymlvo vxyqpwlo
+szksgkq amjsugnyx kecqhwe lhsxqkwta
+ypidfxniiv
+tkaqnz tyexcnbzoe ippcre rozbxw
+dujrkjwc xubrjp
+unvvgd
+ivijossm garork rscobbqf
+jmntnaskb qpedzbpkn
+vwkxmmzdrg sxkhchlplq nlutdb mszqzs rbykmbln wpppqc
+wdajuehddk igifiqc hbaaebkzw edskvvmd
+evtvbr enhusod hvcvyr numzjpcpgk yuajybow jcfyjhozb
+bongwag twvohf grpogc
+ofxeqen
+qikwwpzdko
+accqaoqqz njbjtsrna ewhtrr mdjivwg ebllemkrz
+ehpptvgu
+njvpwaft mosokgnw gypkum
+celzhxw ffcjeel
+bkqjuabmvg lalfdjc nxhkbz gynrgfl
+lqfgbkob vlhdkcppq sgfdtblub ecrjaiadh eiqezqoc
+sskeho pvdnppdb sqfwnae utaetwz hmlrodqtqz
